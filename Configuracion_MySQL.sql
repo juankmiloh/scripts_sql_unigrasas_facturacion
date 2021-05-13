d1d8cddd754c6a18eee9753a5ec878d81709ea5b
juankmiloh <juankmiloh@hotmail.com>
2021-05-13 12:13:43 -0500

    Daño en la base de datos - Se actualiza con valores de storage

--- Configuracion_MySQL.sql
+++ Configuracion_MySQL.sql
@@ -1,4 +1,37 @@
 ----------------------------
+-- COMANDO PARA INSTALAR MYSQL EN LINUX
+----------------------------
+-- https://www.digitalocean.com/community/tutorials/como-instalar-mysql-en-ubuntu-18-04-es
+-- 1. sudo apt update
+-- 2. sudo apt install mysql-server
+-- 3. sudo mysql
+-- 4. SELECT user,authentication_string,plugin,host FROM mysql.user;
+-- 5. ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
+-- 6. FLUSH PRIVILEGES;
+-- 7. SELECT user,authentication_string,plugin,host FROM mysql.user;
+
+----------------------------
+-- COMANDOS PARA HABILITAR ACCESO REMOTO EN MYSQL
+----------------------------
+-- 1. Para habilitarlo, edita el archivo de configuración /etc/mysql/mysql.conf.d/mysqld.cnf --
+-- sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+-- 2. Bajo la entrada [mysqld], comenta las entradas «bind-address» y «skip-external-locking» añadiendo # al principio. --
+-- Debe quedar así:
+    -- [mysqld]
+    -- ...
+    -- #skip-external-locking
+    -- #bind-address 127.0.0.1
+    -- ...
+-- Guarda y cierra.
+-- 3. Solo queda reiniciar el servicio mysql --
+-- sudo /etc/init.d/mysql restart
+
+----------------------------
+-- COMANDO PARA DES-INSTALAR MYSQL EN LINUX
+----------------------------
+-- https://cursoprogramador.wordpress.com/2016/05/01/desinstalar-completamente-mysql-en-ubuntu/
+
+----------------------------
 -- COMANDO PARA ABRIR MYSQL EN LINUX
 ----------------------------
 -- sudo mysql -u root -p
@@ -15,15 +48,15 @@
 
 ----------------------------
 -- COMANDO PARA CREAR USUARIO MYSQL QUE PERMITA CONEXIONES AL SERVIDOR DE BASE DE DATOS DESDE OTROS EQUIPOS
--- SISTEMA OPERATIVO WINDOWS
+-- SISTEMA OPERATIVO WINDOWS - LINUX
 ----------------------------
 SELECT user,authentication_string,plugin,host FROM mysql.user; -- Privilegios de usuario
 CREATE USER 'root'@'%' IDENTIFIED BY '123456';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 -- Borrar usuario
-drop user '<user>'@'<hostname>';
 drop user 'root'@'localhost';
+drop user '<user>'@'<hostname>';
 ---------------------------------------------------------------------------
 -- permitir conexiones remotas mysql ubuntu (documentacion)
 -- https://www.altaruru.com/habilitar-acceso-remoto-a-mysql/
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## COMANDO PARA CREAR BACKUP DE BASE DE DATOS MYSQL
 ------------------------------------------------
 ```
-mysqldump -u root -p unigrasas > /home/azureuser/unigrasas.sql
+mysqldump -u root -p UNIGRASAS > /home/azureuser/scripts_sql_unigrasas_facturacion/backups/2unigrasas13052021.sql
 ```
 
 ## COMANDO PARA RESTAURAR BACKUP BASE DE DATOS MYSQL
@@ -11,9 +11,12 @@ mysqldump -u root -p unigrasas > /home/azureuser/unigrasas.sql
 > Primero crear la base de datos en MySQL y luego si se carga...
 ```
 create database UNIGRASAS;
+
 mysql -u root -p UNIGRASAS < /home/azureuser/scripts_sql_unigrasas_facturacion/DDL.sql
 ```
 > Comando para insertar datos
 ```
 mysql -u root -p UNIGRASAS < /home/azureuser/scripts_sql_unigrasas_facturacion/DML.sql
 ```
+
+<!-- ojo - Crear repositorio con backups -->
